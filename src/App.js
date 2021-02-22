0f6586aa46abecb5b83185481d88cd8780967b75
achiyahb <achiyahb@gmail.com>
2021-02-22 04:56:13 +0200

    set the checkWinning function to be more generic

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 import Row from './components/row'
 
 function App() {
-    const [winningHm, setWinningHm] = useState({hor: {}, ver: {}, dig: {}})
+    const [winningHm, setWinningHm] = useState({hor: {}, ver: {}, dia: {}})
     const [rows, setRows] = useState([[0, 1, 2], [0, 1, 2], [0, 1, 2]])
     const [exTurn, setExTurn] = useState(true)
     const [winner, setWinner] = useState('')
@@ -18,79 +18,56 @@ function App() {
     }
 
     const checkWinning = (square) => {
+        console.time()
         const shape = exTurn ? 'X' : 'O'
+        let checkHm = winningHm
 
         // check horizontal lines
-        let checkHm = winningHm
-        if (!checkHm["hor"][square.rowIndex]) {
-            checkHm["hor"][square.rowIndex] = {}
-            checkHm["hor"][square.rowIndex].i = 1
-            checkHm["hor"][square.rowIndex].firstShape = shape
-            checkHm["hor"][square.rowIndex]['winPossible'] = true
-        } else {
-            if (checkHm["hor"][square.rowIndex]['winPossible'] && checkHm["hor"][square.rowIndex].firstShape === shape) {
-                checkHm["hor"][square.rowIndex].i++
-            } else {
-                checkHm["hor"][square.rowIndex]['winPossible'] = false
-            }
-            if (checkHm["hor"][square.rowIndex].i === 3) {
-                winnerCall(shape)
-            }
-        }
+        let section = 'hor'
+        checkHm = buildHm(checkHm,shape,square.rowIndex,section)
 
         // check vertical lines
-        if (!checkHm["ver"][square.col]) {
-            checkHm["ver"][square.col] = {}
-            checkHm["ver"][square.col].i = 1
-            checkHm["ver"][square.col].firstShape = shape
-            checkHm["ver"][square.col]['winPossible'] = true
-        } else {
-            if (checkHm["ver"][square.col]['winPossible'] && checkHm["ver"][square.col].firstShape === shape) {
-                checkHm["ver"][square.col].i++
-            } else {
-                checkHm["ver"][square.col]['winPossible'] = false
-            }
-            if (checkHm["ver"][square.col].i === 3) {
-                winnerCall(shape)
-            }
-        }
+        section = 'ver'
+        checkHm = buildHm(checkHm,shape,square.col,section)
 
-        //
+        // check diagonally
+        section = 'dia'
         if (square.rowIndex === 0) {
             if (square.col === 0) {
-                checkHm = checkDig(checkHm, shape, 'left')
+                checkHm = buildHm(checkHm, shape, 'left',section)
             } else if (square.col === 2) {
-                checkHm = checkDig(checkHm, shape, 'right')
+                checkHm = buildHm(checkHm, shape, 'right',section)
             }
         } else if (square.rowIndex === 2) {
             if (square.col === 0) {
-                checkHm = checkDig(checkHm, shape, 'right')
+                checkHm = buildHm(checkHm, shape, 'right',section)
             } else if (square.col === 2) {
-                checkHm = checkDig(checkHm, shape, 'left')
+                checkHm = buildHm(checkHm, shape, 'left',section)
             }
         } else {
             if (square.col === 1) {
-                checkHm = checkDig(checkHm, shape, 'right')
-                checkHm = checkDig(checkHm, shape, 'left')
+                checkHm = buildHm(checkHm, shape, 'right',section)
+                checkHm = buildHm(checkHm, shape, 'left',section)
             }
         }
         setWinningHm(checkHm)
         setExTurn(!exTurn)
+        console.timeEnd()
     }
 
-    const checkDig = (checkHm, shape, dir) => {
-        if (!checkHm["dig"][dir]) {
-            checkHm["dig"][dir] = {}
-            checkHm["dig"][dir].i = 1
-            checkHm["dig"][dir].firstShape = shape
-            checkHm["dig"][dir]['winPossible'] = true
+    const buildHm = (checkHm, shape, index,section) => {
+        if (!checkHm[section][index]) {
+            checkHm[section][index] = {}
+            checkHm[section][index].i = 1
+            checkHm[section][index].firstShape = shape
+            checkHm[section][index]['winPossible'] = true
         } else {
-            if (checkHm["dig"][dir]['winPossible'] && checkHm["dig"][dir].firstShape === shape) {
-                checkHm["dig"][dir].i++
+            if (checkHm[section][index]['winPossible'] && checkHm[section][index].firstShape === shape) {
+                checkHm[section][index].i++
             } else {
-                checkHm["dig"][dir]['winPossible'] = false
+                checkHm[section][index]['winPossible'] = false
             }
-            if (checkHm["dig"][dir].i === 3) {
+            if (checkHm[section][index].i === 3) {
                 winnerCall(shape)
             }
         }
@@ -99,6 +76,7 @@ function App() {
 
     const winnerCall = (shape) => {
         setWinner('the winner is ' + shape)
+
     }
 
     return (
